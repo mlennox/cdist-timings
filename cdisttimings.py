4a5ff8cda60f589de94fd96627f6235b8b0dd006
Mark Lennox <mark@webpusher.ie>
2018-11-04 23:01:28 +0000

    adding docs and explanations to code

--- cdisttimings.py
+++ cdisttimings.py
@@ -3,8 +3,18 @@ from numpy import reshape, array, argmin, random
 import timeit
 
 
-def setup(dimension):
-    vector_length = 100
+def setup(dimension, vector_length):
+    """
+    Generates two arrays filled with random values of shape (dimension, vector_length)
+    
+    Arguments:
+      dimension {int} -- the number of rows - this is meant to represent the number of word embeddings
+      vector_length {int} -- the length of the word embedding vector
+    
+    Returns:
+      tuple of the two arrays and the listified array for the loop implementation
+    """
+
     one = random.rand(dimension, vector_length)
     two = random.rand(dimension, vector_length)
     one_array = [reshape(row, (1, -1)) for row in one]
@@ -48,35 +58,56 @@ def vectorised(one, two, dimension):
 
 
 def run():
-    loops = 10
-    repeats = 100
+    """
+    We run our timings `loops` times generating a new set of matrices each timing loop
+    The timings are repeated with the same data `repeats` times
+    We generate matrices that are
+    """
+
+    # how many times we create new test data and time the implementations
+    loops = 2
+    #
+    repeats = 30
     dimension = 1000
-    loops_avgs = []
-    vectorised_avgs = []
+    vector_length = 100
+    loop_based_duration_averages = []
+    vectorised_duration_averages = []
 
     print("Starting benchmarks, running {0} loops".format(loops))
 
-    for loops_index in range(1, loops):
-        print("Timing run {0}".format(loops_index))
+    for loops_index in range(0, loops):
+        print("Timing run {0}".format(loops_index + 1))
 
         # we create a new set of test data each loop and pass same to both implementations
-        one, one_array, two = setup(dimension)
+        one, one_array, two = setup(dimension, vector_length)
 
         print("running loop implementation")
         timer = timeit.Timer(loop_based(one_array, two))
-        duration_loop = timer.repeat(repeat=loops, number=1)
-        # print("durations", duration_loop)
-        loop_avg = sum(duration_loop) / len(duration_loop)
-        print("average loop duration = {0}".format(loop_avg))
+        # repeat the timing `repeats` times but only execute function once per repeat
+        duration_loop = timer.repeat(repeat=repeats, number=1)
+        # duration_loop will be a list of timings of length `repeats`
+        loop_based_duration_averages.extend(duration_loop)
+        print(
+            "average loop duration = {0}".format(
+                sum(duration_loop) / len(duration_loop)
+            )
+        )
 
-        print("running vectorised - - - - - - -")
+        print("running vectorised implementation")
         timer = timeit.Timer(vectorised(one, two, dimension))
-        duration_vector = timer.repeat(repeat=loops, number=1)
-        # print("durations", duration_vector)
-        vectorised_avg = sum(duration_vector) / len(duration_vector)
-        print("average vectorised duration = {0}".format(vectorised_avg))
+        duration_vector = timer.repeat(repeat=repeats, number=1)
+        vectorised_duration_averages.extend(duration_vector)
+        print(
+            "average vectorised duration = {0}".format(
+                sum(duration_vector) / len(duration_vector)
+            )
+        )
 
-        print("vectorised is {0} times faster".format(loop_avg / vectorised_avg))
+    loop_avg = sum(loop_based_duration_averages) / len(loop_based_duration_averages)
+    vectorised_avg = sum(vectorised_duration_averages) / len(
+        vectorised_duration_averages
+    )
+    print("vectorised is {0} times faster".format(loop_avg / vectorised_avg))
 
 
 run()
