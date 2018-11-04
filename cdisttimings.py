d3338964b667c6fa170be8446d9d9a55054e6076
Mark Lennox <mark@webpusher.ie>
2018-11-04 18:13:54 +0000

    stubbing out the script

---
+++ cdisttimings.py
@@ -0,0 +1,16 @@
+from scipy import spatial
+
+
+def loop():
+    print("running loop")
+
+
+def vectorised():
+    print("running vectorised")
+
+
+def run():
+    print("Starting benchmarks")
+
+
+run()
